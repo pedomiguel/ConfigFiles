136ea196bbe0f1ed48c50ea03fd4747746847c8c
synthbe <108361383+synthbe@users.noreply.github.com>
2026-02-23 23:29:42 -0300

    fix check_upgrades, workspace independencies

--- waybar/scripts/check_upgrades.sh
+++ waybar/scripts/check_upgrades.sh
@@ -2,8 +2,8 @@
 
 UPDATES=$(dnf check-update -q 2>/dev/null | grep -E '^[a-zA-Z0-9]' | wc -l)
 
-if [ "$UPDATES" -eq 0 ]; then
-    echo '{"text":"󰇚 0","class":"up-to-date"}'
+if [ "$UPDATES" -gt 0 ]; then
+  printf '{"text": "%s", "alt": "%s", "tooltip": "%s Updates"}' "$UPDATES" "$UPDATES" "$UPDATES"
 else
-    echo "{\"text\":\"󰇚 $UPDATES\",\"class\":\"updates-available\"}"
+  printf '{"text": "0", "alt": "0", "tooltip": "0 Updates"}'
 fi
